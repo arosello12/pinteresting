240271821f0b8f4326666768fdd9081e107c68d6
Alfred Rosello <alrosello@fusionworlds.com>
2015-08-01 22:06:25 -0400

    Cleaned up the pins scaffold

---
+++ app/controllers/pins_controller.rb
@@ -0,0 +1,67 @@
+class PinsController < ApplicationController
+  before_action :set_pin, only: [:show, :edit, :update, :destroy]
+
+  # GET /pins
+  #Read All
+  def index
+    @pins = Pin.all
+  end
+
+  # GET /pins/1
+  #Read
+  def show
+  end
+
+  # GET /pins/new
+  #Prep For Create
+  def new
+    @pin = Pin.new
+  end
+  
+  # POST /pins
+  # Create
+  def create
+    @pin = Pin.new(pin_params)
+
+    if @pin.save
+      redirect_to @pin, notice: 'Pin was successfully created.'
+    else
+      render :new
+    end
+    
+  end
+
+
+  # GET /pins/1/edit
+  #Prep for Update
+  def edit
+  end
+
+  # PATCH/PUT /pins/1
+  #Update
+  def update
+    if @pin.update(pin_params)
+      redirect_to @pin, notice: 'Pin was successfully updated.'
+    else
+     render :edit
+    end
+  end
+
+  # DELETE /pins/1
+  #Destroy
+  def destroy
+    @pin.destroy
+    redirect_to pins_url, notice: 'Pin was successfully destroyed.'
+  end
+
+  private
+    # Use callbacks to share common setup or constraints between actions.
+    def set_pin
+      @pin = Pin.find(params[:id])
+    end
+
+    # Never trust parameters from the scary internet, only allow the white list through.
+    def pin_params
+      params.require(:pin).permit(:description)
+    end
+end
